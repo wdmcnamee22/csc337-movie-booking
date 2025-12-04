c0b17408d2e2f9382c6242242f5c21b4dce3e5ff
wdmcnamee22 <wdmcnamee@arizona.edu>
2025-12-04 12:53:28 -0700

    Adding in initial server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "337-project-secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Home page placeholder
+app.get("/", function (req, res) {
+    res.send("Home page placeholder");
+});
+
+/* Users Module Routes */
+
+/* Events/Movies Module Routes */
+
+/* Bookings Module Routes */
+
+app.get("/bookings", function (req, res) {
+    res.send("My bookings page placeholder");
+});
+
+app.post("/bookings", function (req, res) {
+    console.log("POST /bookings received");
+    console.log("userId:", req.session.userId);
+    console.log("eventId:", req.body.eventId);
+
+    res.redirect("/bookings");
+});
+
+/* Review Module Routes */
+
+const PORT = 3000;
+app.listen(PORT, function () {
+    console.log("Server running on port", PORT);
+});
